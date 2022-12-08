8b3c786aa84235a892102f6a93ae72645f9134af
OneBitPython <ananthramvijayaraj@gmail.com>
2022-12-08 10:42:00 +0530

    committed day 8. So close to top 100

---
+++ 2022/day8/pt2.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+const double PI=3.141592653589;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+
+
+void solve()
+{
+    int n =99;
+    int m = n;
+    vector<vector<int>>a(n+1, vector<int>(m+1));
+    for(int i = 1;i<=n;++i){
+        string s;
+        cin >> s;
+        for(int j = 1;j<=m;++j){
+            int x = s[j-1]-'0';
+            a[i][j] = x;
+        }
+    }
+    int res = 0;
+    for(int i = 2;i<n;++i){
+        for(int j = 2;j<m;++j){
+            int mx = 0;
+            int cntu = 0, cntd = 0, cntl = 0, cntr = 0;
+            for(int k = i-1;k>=1;--k){
+                cntu++;
+                if(a[k][j] >= a[i][j])break;
+            }
+            for(int k = i+1;k<=n;++k){
+                cntd++;
+                if(a[k][j] >= a[i][j])break;
+            }
+            for(int k = j-1;k>=1;--k){
+                cntl++;
+                if(a[i][k] >= a[i][j])break;
+            }
+            for(int k = j+1;k<=m;++k){
+                cntr++;
+                if(a[i][k] >= a[i][j])break;
+                mx = max(mx, a[i][k]);
+            }
+            res = max(res, cntl*cntr*cntd*cntu);
+        }
+    }
+    cout << res;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    for(int i = 1;i<=T;++i)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
